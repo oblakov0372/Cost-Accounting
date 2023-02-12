19902ca94ea5da5a3bea49383f9c69acf60ca360
Demian <demaoblak1@gmail.com>
2023-02-12 19:25:34 +0300

    Create entities user,cost,category

---
+++ CostAccounting.Api/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CostAccounting.Api.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+    }
+}
---
+++ CostAccounting.Api/Entities/Cost.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CostAccounting.Api.Entities
+{
+    public class Cost
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CategoryId { get; set; }
+        public decimal Price { get; set; }
+        public string Comment { get; set; }
+
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+
+    }
+}
